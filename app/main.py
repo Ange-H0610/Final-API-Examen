2c02b7928de26c8325a3b86a247cb6506f99d9e9
Ange-H0610 <hei.iraka.4@gmail.com>
2025-09-03 09:57:23 +0300

    créez des différents routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, API Exam!"}
+class Characteristic(BaseModel):
+    ram_memory: int
+    rom_memory: int
+
+
+class Phone(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+
+phones_db: List[Phone] = []
+
+# a.
+@app.get("/health")
+def health_check():
+    return "Ok"
+
+# b.
+@app.post("/phones", status_code=201)
+def create_phones(phones: List[Phone]):
+    for phone in phones:
+        phones_db.append(phone)
+    return {"message": "Phones added successfully"}
+
+# c.
+@app.get("/phones")
+def get_phones():
+    return phones_db
+
+# d.
+@app.get("/phones/{id}")
+def get_phone(id: str):
+    for phone in phones_db:
+        if phone.identifier == id:
+            return phone
+    
